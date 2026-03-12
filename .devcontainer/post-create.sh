638aa3d4920fc2f783b158aed6394b5b1e1b8e6d
Drew Clarke <drew.clarke79@gmail.com>
2026-03-12 16:23:39 +0000

    More updates

--- .devcontainer/post-create.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-## Add any post-create commands here 
-## Ex: any npm dependencies we want image-wide
-
-## Launch the blog app
-cd blog; npm install && npx next telemetry disable && npm run build
